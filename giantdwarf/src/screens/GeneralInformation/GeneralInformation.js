a85e1427af309e98e6acb7599e5ddf28b934ae6a
zlilyoffe <zlilyoffe@gmail.com>
2022-02-14 14:12:17 +0200

    css general info page

--- giantdwarf/src/screens/GeneralInformation/GeneralInformation.js
+++ giantdwarf/src/screens/GeneralInformation/GeneralInformation.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './GeneralInformation.css';
+ 
 
 
 const GeneralInformation = () => {
 
   return (
-    <div className="joinOuterContainer">
-      <div className="joinInnerContainer">
-        <h1 className="heading">General Information</h1>
-        <div>
+    <div className="generalOuterContainer">
+      <div className="generalInnerContainer">
+        <h1 className="heading2">General <br></br> Information.</h1>
+        <div className="generalText">
             All you have to do is sign up and join a group  (or create one).
             With the help of the app, you will get all the details you need about your giant!
             soooo let’s begin!
         </div>
         <Link to={`/SignUp`}>
-          <button className={'button mt-20'} type="submit">lets begin</button>
+          <button className={'button'} type="submit">lets begin</button>
         </Link>
       </div>
     </div>
