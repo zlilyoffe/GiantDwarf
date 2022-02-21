423e0a21bb77f10f9e2e592e7049efac9737148b
zlilyoffe <zlilyoffe@gmail.com>
2022-02-21 12:53:17 +0200

    handleError in logic-home

--- server/screens/Play/database.js
+++ server/screens/Play/database.js
@@ -2,11 +2,7 @@ import { groupSchema, groupSchemaModel } from '../createNewGroup/database.js';
 import mongoose from 'mongoose';
 
 export const getGroupName = async (groupId) => {
-  try {
-    const Group = groupSchemaModel;
-    const res = await Group.findOne({ _id: groupId });
-    return res;
-  } catch (e) {
-    return handleError(e);
-  }
+  const Group = groupSchemaModel;
+  const res = await Group.findOne({ _id: groupId });
+  return res;
 };
