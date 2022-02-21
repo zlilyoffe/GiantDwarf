12a5c46d1077b9e8172909dc0169b42e7da6430f
zlilyoffe <zlilyoffe@gmail.com>
2022-02-21 13:39:14 +0200

    database handleError

--- screens/LinkPage/database.js
+++ screens/LinkPage/database.js
@@ -2,11 +2,7 @@ import { groupSchema, groupSchemaModel } from '../createNewGroup/database.js';
 import mongoose from 'mongoose';
 
 export const getLinkByGroup = async (groupId) => {
-  try {
     const Group = groupSchemaModel;
     const res = await Group.findOne({ _id: groupId });
     return res;
-  } catch (e) {
-    return handleError(e);
-  }
 };
