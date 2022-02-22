273b40b9f8420fc05742fe01af14c9b4f4169f68
zlilyoffe <zlilyoffe@gmail.com>
2022-02-22 12:29:54 +0200

    from class component to functional

--- giantdwarf/src/screens/SignIn/SignIn.js
+++ giantdwarf/src/screens/SignIn/SignIn.js
@@ -1,82 +1,65 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-class SignIn extends Component {
-  constructor() {
-    super();
+function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const changePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-    this.state = {
-      email: '',
-      password: '',
-      loginErrors: '',
-    };
-    this.changeEmail = this.changeEmail.bind(this);
-    this.changePassword = this.changePassword.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-  changeEmail(event) {
-    this.setState({
-      email: event.target.value,
-    });
-  }
-  changePassword(event) {
-    this.setState({
-      password: event.target.value,
-    });
-  }
-  async onSubmit(event) {
-    event.preventDefault();
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
     const registered = {
-      email: this.state.email,
-      password: this.state.password,
+      email: email,
+      password: password,
     };
-    var signInResult = {};
     const response = await axios.post(
       'http://localhost:4000/api/SignIn',
       registered
     );
-    signInResult = response.data;
-       if (signInResult.userExists === true) {
-      // history.push('/Home');
-      // window.location = '/Home';
-      console.log('signInRESSSSS', response);
+    const signInResult = response.data;
+    if (signInResult.userExists === true) {
+      history.push('/Home');
     } else {
       window.location = '/';
     }
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="container">
-          <div className="form-div">
-            <form onSubmit={this.onSubmit}>
-              <input
-                type="text"
-                placeholder="E-mail"
-                onChange={this.changeEmail}
-                value={this.state.email}
-                className="form-control form-group"
-              />
-              <input
-                type="text"
-                placeholder="Password"
-                onChange={this.changePassword}
-                value={this.state.password}
-                className="form-control form-group"
-              />
-              <input
-                type="submit"
-                className="btn btn-danger btn-block"
-                value="Submit"
-              />
-            </form>
-          </div>
+  };
+  return (
+    <div>
+      <div className="container">
+        <div className="form-div">
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="E-mail"
+              onChange={changeEmail}
+              value={email}
+              className="form-control form-group"
+            />
+            <input
+              type="text"
+              placeholder="Password"
+              onChange={changePassword}
+              value={password}
+              className="form-control form-group"
+            />
+            <input
+              type="submit"
+              className="btn btn-danger btn-block"
+              value="Submit"
+            />
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SignIn;
