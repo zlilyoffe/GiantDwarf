541f4cec13a0f6fc6538cd61cc5121f600bc0cb9
zlilyoffe <zlilyoffe@gmail.com>
2022-02-22 12:02:53 +0200

    console.log Dbag

--- screens/Home/logic.js
+++ screens/Home/logic.js
@@ -1,11 +1,13 @@
 import { getGroupByCode } from './database.js';
+// import { getGroupById } from './database.js';
 
-export const signedInGroup = async (code) => {
+export const signedInGroup = async (code, userId) => {
   console.log('signedInGroup');
   console.log(code);
+  console.log(userId);
   try {
-    const group = await getGroupByCode(code);
-    console.log(group);
+    const group = await getGroupByCode(code, userId);
+    console.log('my group:', group);
     if (!group) {
       return {
         groupExists: false,
@@ -18,3 +20,29 @@ export const signedInGroup = async (code) => {
     return { massage: 'ooppsss please try again' };
   }
 };
+
+// export const mergeGroupUsers = async (groupId, userId) => {
+//   try {
+//     const newGroup = await getGroupById(groupId, userId);
+//     if (!newGroup) {
+//       return { error: 'oppsss there is problame' };
+//     }
+//     if (groupId == group.groupId);
+//     return { groupExists: true };
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// export const signedInUser = async (email, password) => {
+//   try {
+//     const user = await getUserByEmail(email, password);
+//     if (!user) {
+//       return { userExists: false, error: 'User does not exist' };
+//     }
+//     const match = await bcrypt.compare(password, user.password);
+//     return { userExists: match ? true : false };
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
