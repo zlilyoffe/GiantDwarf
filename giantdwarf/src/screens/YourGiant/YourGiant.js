099e19bb17cbdd9b02644bf7bd86c4230907a6b8
zlilyoffe <zlilyoffe@gmail.com>
2022-03-02 09:46:14 +0200

    play button on play screen, mobx

--- giantdwarf/src/screens/YourGiant/YourGiant.js
+++ giantdwarf/src/screens/YourGiant/YourGiant.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import { observer } from 'mobx-react-lite';
+import React, { useState } from 'react';
+import { useStore } from '../../helpers/useStore';
 
 const YourGiant = () => {
+  const store = useStore();
+  const [giant, setGiant] = useState('');
+  React.useEffect(() => {
+    (async () => {
+      const response = await axios.post('http://localhost:4000/api/myGiant', {
+        groupId: store.currentGroupId,
+      });
+      console.log(response.data);
+      console.log(store.currentGroupId);
+      setGiant(response.data.giant);
+    })();
+  }, []);
   return (
     <div className="yourGiantOuterContainer">
       <div className="yourGiantInnerContainer">
         <h1 className="heading">countdown begun</h1>
-        <div>your giant is:</div>
+        <div>your giant is:{giant}</div>
         <div> her address::</div>
         <div>general:</div>
         <h2>Have Fun!!</h2>
--- server/screens/Play/database.js
+++ server/screens/Play/database.js
@@ -5,5 +5,6 @@ export const getGroupName = async (groupId) => {
   console.log('bla', groupId);
   const Group = groupSchemaModel;
   const res = await Group.findOne({ _id: groupId });
+  console.log('group info', res);
   return res;
 };
