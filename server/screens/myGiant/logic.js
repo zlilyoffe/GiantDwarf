bd204f39e27123d1677fb32280f55127dd611f02
zlilyoffe <zlilyoffe@gmail.com>
2022-03-13 21:30:34 +0200

    get participants to create list of giant-dwarf

--- server/screens/myGiant/logic.js
+++ server/screens/myGiant/logic.js
@@ -2,9 +2,16 @@ import { groupSchema } from '../createNewGroup/database.js';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import { getGiantByGroup } from './database.js';
+import { getGroup } from '../Play/database.js';
 
 export const giantInfo = async (groupId) => {
   try {
+    const group = await getGroup(groupId);
+    if (!group) {
+      return { groupExists: false, error: 'group does not exist' };
+    }
+    const participants = group.participants;
+    ////
     const giant = await getGiantByGroup(groupId);
     if (!giant) {
       return { groupExists: false, error: 'giant does not exist' };
