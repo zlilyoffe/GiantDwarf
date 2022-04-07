4d7f651fceb638696700ed084ca8f9b1e1c806b2
zlilyoffe <zlilyoffe@gmail.com>
2022-04-07 18:10:25 +0300

    lago in logic

--- server/screens/myGiant/logic.js
+++ server/screens/myGiant/logic.js
@@ -10,8 +10,21 @@ export const giantInfo = async (groupId) => {
     if (!group) {
       return { groupExists: false, error: 'group does not exist' };
     }
-    const participants = group.participants;
+    // const participants = group.participants;
     ////
+    for (let participantsNumber = group.participants.length; participantsNumber > 0; participantsNumber-- ){
+      function getRandom(){
+        let numDwarf = (Math.random() * (participantsNumber + 1 - 0) + 0);
+        let numGiant = (Math.random() * (participantsNumber + 1 - 0) + 0);
+      } if (numDwarf != numGiant){
+        giantArray.splice(index, numGiant, group.participants[participantsNumber]);
+        dwarfArray.splice(index, numDwarf, group.participants[participantsNumber]);
+      } else {
+        let numDwarf = (Math.random() * (participantsNumber + 1 - 0) + 0);
+        giantArray.splice(index, numGiant, group.participants[participantsNumber]);
+        dwarfArray.splice(index, numDwarf, group.participants[participantsNumber]);
+      }
+    }
     const giant = await getGiantByGroup(groupId);
     if (!giant) {
       return { groupExists: false, error: 'giant does not exist' };
